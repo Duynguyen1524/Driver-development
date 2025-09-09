d3be673974c49f7be7293ec2afb7c1797999def5
Duynguyen1524 <132160865+Duynguyen1524@users.noreply.github.com>
2025-09-09 18:03:29 -0400

    Update i2c_stm32f4xx_driver.h

--- workspaceforselflearn/stm32F401RE_Driver/drivers/Inc/i2c_stm32f4xx_driver.h
+++ workspaceforselflearn/stm32F401RE_Driver/drivers/Inc/i2c_stm32f4xx_driver.h
@@ -97,6 +97,13 @@ void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pTxbuffer, uint32_
 uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr);
 uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle,uint8_t *pRxBuffer, uint8_t Len, uint8_t SlaveAddr,uint8_t Sr);
 
+
+void I2C_CloseReceiveData(I2C_Handle_t *pI2CHandle);
+void I2C_CloseSendData(I2C_Handle_t *pI2CHandle);
+
+
+void I2C_SlaveSendData(I2C_RegDef_t *pI2C,uint8_t data);
+uint8_t I2C_SlaveReceiveData(I2C_RegDef_t *pI2C);
 void I2C_EV_IRQHandling(I2C_Handle_t *pI2CHandle);
 
 void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi );
